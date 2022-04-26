b9a1515471034f51b36a9c6df3d4571a6f7d65b1
NielsVollebregt <niels.vollebregt@student.hu.nl>
2022-04-26 11:21:55 +0200

    API code Franc
    API code straight from canvas. VGA_CubeIDE_2021.zip

---
+++ VGA_CubeIDE_2021.zip_expanded/VGA/Core/Src/main.c
@@ -0,0 +1,30 @@
+//--------------------------------------------------------------
+// File     : main.c
+// Datum    : 30.03.2016
+// Version  : 1.0
+// Autor    : UB
+// mods by	: J.F. van der Bent
+// CPU      : STM32F4
+// IDE      : CooCox CoIDE 1.7.x
+// Module   : CMSIS_BOOT, M4_CMSIS_CORE
+// Function : VGA_core DMA LIB 320x240, 8bit color
+//--------------------------------------------------------------
+
+#include "main.h"
+#include "stm32_ub_vga_screen.h"
+#include <math.h>
+
+
+int main(void)
+{
+	SystemInit(); // System speed to 168MHz
+
+	UB_VGA_Screen_Init(); // Init VGA-Screen
+
+	UB_VGA_FillScreen(VGA_COL_BLUE);
+
+  while(1)
+  {
+
+  }
+}
