a2100840248b8502bebf3b98e48efce5f3a43abe
samantha-blonk <93520755+samantha-blonk@users.noreply.github.com>
2022-04-28 13:13:03 +0200

    Frontlayer.c & Frontlayer.H aangemaakt
    Front layer.c en Frontlayer.h aangemaakt

---
+++ Swont_ide_uart_6.5.zip_expanded/swont_ide/Core/Inc/Frontlayer.h
@@ -0,0 +1,13 @@
+/*
+ * Frontlayer.h
+ *
+ *  Created on: 28 Apr 2022
+ *      Author: saman
+ */
+
+#ifndef INC_FRONTLAYER_H_
+#define INC_FRONTLAYER_H_
+
+
+
+#endif /* INC_FRONTLAYER_H_ */
---
+++ Swont_ide_uart_6.5.zip_expanded/swont_ide/Core/Src/Frontlayer.c
@@ -0,0 +1,8 @@
+/*
+ * Frontlayer.c
+ *
+ *  Created on: 28 Apr 2022
+ *      Author: saman
+ */
+
+
